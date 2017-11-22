b756f6fabfc9b86f918cf507f61412da37cdfe9e
DeflatedPickle <DeflatedPickle@gmail.com>
2017-11-22 18:51:34 +0000

    Updated Python interpreter

--- StuckInTrouble/Assets/Scripts/PythonInterpreter.cs
+++ StuckInTrouble/Assets/Scripts/PythonInterpreter.cs
@@ -6,15 +6,23 @@ using UnityEngine;
 
 public class PythonInterpreter : MonoBehaviour {
 	private ScriptEngine _pythonScript;
+	private ScriptScope _pythonScope;
 
 	private void Start () {
 		_pythonScript = Python.CreateEngine();
+		_pythonScope = _pythonScript.CreateScope();
 
-		var pythonCode = "import sys;print sys.version";
+		var pythonCode = "import sys;print 'Python Version: %s' % sys.version";
 		Run(pythonCode);
 	}
 
 	public object Run(string pythonCode) {
-		return _pythonScript.Execute(pythonCode);
+		return _pythonScript.Execute(pythonCode, _pythonScope);
+	}
+
+	public void Log(string pythonCode) {
+		var result = Run(pythonCode);
+		
+		Debug.Log(result.ToString());
 	}
 }
