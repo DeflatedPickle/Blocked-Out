a5d36b52196c30a516292bf7164d712c01e9af3b
Dibbo <DeflatedPickle@gmail.com>
2017-11-22 16:00:59 +0000

    Added a Python interpreter

---
+++ StuckInTrouble/Assets/Scripts/PythonInterpreter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using UnityEngine;
+
+public class PythonInterpreter : MonoBehaviour {
+	private ScriptEngine _pythonScript;
+
+	private void Start () {
+		_pythonScript = Python.CreateEngine();
+
+		var pythonCode = "import sys;print sys.version";
+		Run(pythonCode);
+	}
+
+	public object Run(string pythonCode) {
+		return _pythonScript.Execute(pythonCode);
+	}
+}
