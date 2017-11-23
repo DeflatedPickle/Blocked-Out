945c9bea18db51967723a60509197bb753151c66
DeflatedPickle <DeflatedPickle@gmail.com>
2017-11-23 18:53:29 +0000

    Added name spaces to interpreters

--- StuckInTrouble/Assets/Scripts/Interpreters/LuaInterpreter.cs
+++ StuckInTrouble/Assets/Scripts/Interpreters/LuaInterpreter.cs
@@ -1,38 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using MoonSharp.Interpreter;
+﻿using MoonSharp.Interpreter;
 using MoonSharp.Interpreter.Loaders;
 using UnityEngine;
 
-public class LuaInterpreter {
-    private static readonly Script LuaScript;
+namespace Interpreters {
+    public class LuaInterpreter {
+        private static readonly Script LuaScript;
 
-    static LuaInterpreter() {
-        Script.DefaultOptions.ScriptLoader = new EmbeddedResourcesScriptLoader();
-        Script.DefaultOptions.DebugPrint = Debug.Log;
+        static LuaInterpreter() {
+            Script.DefaultOptions.ScriptLoader = new EmbeddedResourcesScriptLoader();
+            Script.DefaultOptions.DebugPrint = Debug.Log;
 
-        LuaScript = new Script();
-    }
+            LuaScript = new Script();
+        }
 
-    public DynValue Run(string luaCode) {
-        return LuaScript.DoString(luaCode);
-    }
+        public DynValue Run(string luaCode) {
+            return LuaScript.DoString(luaCode);
+        }
 
-    public void Log(string luaCode) {
-        var result = Run(luaCode);
+        public void Log(string luaCode) {
+            var result = Run(luaCode);
 
-        switch (result.Type) {
-            case DataType.String:
-                Debug.Log(result.String);
-                break;
+            switch (result.Type) {
+                case DataType.String:
+                    Debug.Log(result.String);
+                    break;
 
-            case DataType.Number:
-                Debug.Log(result.Number);
-                break;
+                case DataType.Number:
+                    Debug.Log(result.Number);
+                    break;
 
-            case DataType.Boolean:
-                Debug.Log(result.Boolean);
-                break;
+                case DataType.Boolean:
+                    Debug.Log(result.Boolean);
+                    break;
+            }
         }
     }
 }
--- StuckInTrouble/Assets/Scripts/Interpreters/PythonInterpreter.cs
+++ StuckInTrouble/Assets/Scripts/Interpreters/PythonInterpreter.cs
@@ -1,50 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Text;
 using IronPython.Hosting;
 using Microsoft.Scripting.Hosting;
 using UnityEngine;
 
-public class PythonInterpreter {
-    private static readonly ScriptEngine PythonScript;
-    private static readonly ScriptScope PythonScope;
-    private static MemoryStream _memoryStream;
+namespace Interpreters {
+    public class PythonInterpreter {
+        private static readonly ScriptEngine PythonScript;
+        private static readonly ScriptScope PythonScope;
+        private static MemoryStream _memoryStream;
 
-    static PythonInterpreter() {
-        PythonScript = Python.CreateEngine();
-        PythonScope = PythonScript.CreateScope();
-    }
+        static PythonInterpreter() {
+            PythonScript = Python.CreateEngine();
+            PythonScope = PythonScript.CreateScope();
+        }
 
-    public object Run(string pythonCode) {
-        _memoryStream = new MemoryStream();
-        PythonScript.Runtime.IO.SetOutput(_memoryStream, new StreamWriter(_memoryStream));
+        public object Run(string pythonCode) {
+            _memoryStream = new MemoryStream();
+            PythonScript.Runtime.IO.SetOutput(_memoryStream, new StreamWriter(_memoryStream));
 
-        var result = PythonScript.Execute(pythonCode, PythonScope);
+            var result = PythonScript.Execute(pythonCode, PythonScope);
 
-        Debug.Log(ReadFromStream(_memoryStream));
-        _memoryStream.Close();
+            Debug.Log(ReadFromStream(_memoryStream));
+            _memoryStream.Close();
 
-        return result;
-    }
+            return result;
+        }
 
-    public void Log(string pythonCode) {
-        var result = Run(pythonCode);
+        public void Log(string pythonCode) {
+            var result = Run(pythonCode);
 
-        Debug.Log(result.ToString());
-    }
+            Debug.Log(result.ToString());
+        }
 
-    /*
+        /*
      * Credit: seshapv
      * Link: https://blogs.msdn.microsoft.com/seshadripv/2008/07/08/how-to-redirect-output-from-python-using-the-dlr-hosting-api/
      */
-    private static string ReadFromStream(MemoryStream stream) {
-        var length = (int) stream.Length;
-        var bytes = new byte[length];
+        private static string ReadFromStream(MemoryStream stream) {
+            var length = (int) stream.Length;
+            var bytes = new byte[length];
 
-        stream.Seek(0, SeekOrigin.Begin);
-        stream.Read(bytes, 0, (int) stream.Length);
+            stream.Seek(0, SeekOrigin.Begin);
+            stream.Read(bytes, 0, (int) stream.Length);
 
-        return Encoding.GetEncoding("utf-8").GetString(bytes, 0, (int) stream.Length - 1);
+            return Encoding.GetEncoding("utf-8").GetString(bytes, 0, (int) stream.Length - 1);
+        }
     }
 }
