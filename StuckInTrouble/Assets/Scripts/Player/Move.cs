c0a507f7583d626baf6994a7f245d840bc5625d2
Dibbo <DeflatedPickle@gmail.com>
2017-12-06 15:51:39 +0000

    Constrained player movement to A and D

--- StuckInTrouble/Assets/Scripts/Player/Move.cs
+++ StuckInTrouble/Assets/Scripts/Player/Move.cs
@@ -15,14 +15,16 @@ public class Move : MonoBehaviour {
 	}
 
 	private void Update () {
-		var horizontal = Input.GetAxis("Horizontal");
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) {
+			var horizontal = Input.GetAxis("Horizontal");
 
-		if (horizontal * _rigidbody2D.velocity.x < MaxSpeed) {
-			_rigidbody2D.AddForce(Vector2.right * horizontal * MoveForce);
-		}
+			if (horizontal * _rigidbody2D.velocity.x < MaxSpeed) {
+				_rigidbody2D.AddForce(Vector2.right * horizontal * MoveForce);
+			}
 
-		if (Mathf.Abs(_rigidbody2D.velocity.x) > MaxSpeed) {
-			_rigidbody2D.velocity = new Vector2(Mathf.Sign(_rigidbody2D.velocity.x) * MaxSpeed, _rigidbody2D.velocity.y);
+			if (Mathf.Abs(_rigidbody2D.velocity.x) > MaxSpeed) {
+				_rigidbody2D.velocity = new Vector2(Mathf.Sign(_rigidbody2D.velocity.x) * MaxSpeed, _rigidbody2D.velocity.y);
+			}
 		}
 	}
 }
