13746bcf1fe4a45ab776af6eac989ecee292f6c3
Dibbo <DeflatedPickle@gmail.com>
2017-12-06 14:59:51 +0000

    Added squashing and stretching

---
+++ StuckInTrouble/Assets/Scripts/Player/Animations.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour {
+	public void Squash(float x, float y, float z = 0) {
+		transform.localScale -= new Vector3(x, y, z);
+	}
+
+	public void Stretch(float x, float y, float z = 0) {
+		transform.localScale += new Vector3(x, y, z);
+	}
+}
--- StuckInTrouble/Assets/Scripts/Player/Jump.cs
+++ StuckInTrouble/Assets/Scripts/Player/Jump.cs
@@ -11,24 +11,35 @@ public class Jump : MonoBehaviour {
 	private bool _isGrounded;
 	
 	private Rigidbody2D _rigidbody2D;
+	private Animations _animations;
 
 	private void Awake() {
 		_rigidbody2D = GetComponent<Rigidbody2D>();
+		_animations = GetComponent<Animations>();
 	}
 
 	private void Update() {
+		
 		if (Input.GetKeyDown(KeyCode.Space) && _isGrounded) {
 			_rigidbody2D.velocity = Vector2.up * JumpVelocity;
 		}
 	}
 
 	private void OnCollisionEnter2D(Collision2D other) {
+		if (!_isGrounded) {
+			_animations.Stretch(0.05f, 0.0f);
+		}
+		
 		if (other.gameObject.CompareTag("Ground")) {
 			_isGrounded = true;
 		}
 	}
 
 	private void OnCollisionExit2D(Collision2D other) {
+		if (_isGrounded) {
+			_animations.Squash(0.05f, 0.0f);
+		}
+		
 		_isGrounded = false;
 	}
 }
