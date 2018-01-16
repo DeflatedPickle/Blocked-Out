360142f9a4b68d1b76b6a8a530f8b3fafa1de82b
Dibbo <DeflatedPickle@gmail.com>
2018-01-16 14:49:09 +0000

    Re-implemented the debug info

--- BlockedOut/Assets/Scripts/Player/Move.cs
+++ BlockedOut/Assets/Scripts/Player/Move.cs
@@ -9,14 +9,21 @@ public class Move : MonoBehaviour {
 	public float MaxSpeed = 5f;
 
 	private Rigidbody2D _rigidbody2D;
+	private Jump _jump;
 
 	private void Awake() {
 		_rigidbody2D = GetComponent<Rigidbody2D>();
+		_jump = GetComponent<Jump>();
 	}
 
 	private void Update () {
 		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) {
 			var horizontal = Input.GetAxis("Horizontal");
+
+			// IsGrounded bug needs to be fixed first.
+			/* if (!_jump.IsGrounded) {
+				MaxSpeed /= 2;
+			} */
 
 			if (horizontal * _rigidbody2D.velocity.x < MaxSpeed) {
 				_rigidbody2D.AddForce(Vector2.right * horizontal * MoveForce);
