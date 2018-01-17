a7dec8aba27f01d7a863215e126b3fb774a2a948
Dibbo <DeflatedPickle@gmail.com>
2018-01-17 11:07:23 +0000

    Added sounds

--- BlockedOut/Assets/Scripts/Player/Jump.cs
+++ BlockedOut/Assets/Scripts/Player/Jump.cs
@@ -12,15 +12,20 @@ public class Jump : MonoBehaviour {
 	
 	private Rigidbody2D _rigidbody2D;
 	private AnimationManager _animationManager;
+	private AudioSource _jump;
+	private AudioSource _fall;
 
 	private void Awake() {
 		_rigidbody2D = GetComponent<Rigidbody2D>();
 		_animationManager = GetComponent<AnimationManager>();
+		_jump = GetComponents<AudioSource>()[0];
+		_fall = GetComponents<AudioSource>()[1];
 	}
 
 	private void Update() {
 		if (Input.GetKeyDown(KeyCode.Space) && IsGrounded) {
 			_rigidbody2D.velocity = Vector2.up * JumpVelocity;
+			_jump.Play();
 		}
 		
 		DebugInfo.AppendDebug(string.Format("Is Grounded? {0}", IsGrounded));
@@ -29,6 +34,7 @@ public class Jump : MonoBehaviour {
 	private void OnCollisionEnter2D(Collision2D other) {
 		if (!IsGrounded) {
 			_animationManager.Stretch(0.05f, 0.0f);
+			_fall.Play();
 		}
 		
 		if (other.gameObject.CompareTag("Ground")) {
--- BlockedOut/Assets/Scripts/RotationController.cs
+++ BlockedOut/Assets/Scripts/RotationController.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class RotationController : MonoBehaviour {
 	private RotationManager _rotationManager;
+	private AudioSource _audioSource;
 
 	private void Awake() {
 		_rotationManager = GetComponent<RotationManager>();
+		_audioSource = GetComponent<AudioSource>();
 	}
 	
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.RightArrow)) {
 			_rotationManager.RotateRight();
+			_audioSource.Play();
 		} else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
 			_rotationManager.RotateLeft();
+			_audioSource.Play();
 		}
 	}
 }
