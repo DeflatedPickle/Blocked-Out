04e4bcedb7b09a8694e8395f1426a94a430d8bee
Dibbo <DeflatedPickle@gmail.com>
2018-01-18 18:46:27 +0000

    Updated level switching and added a label

--- BlockedOut/Assets/Scripts/NextLevel.cs
+++ BlockedOut/Assets/Scripts/NextLevel.cs
@@ -2,13 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NextLevel : MonoBehaviour {
 	public string Scene;
+	[Range(0, 100)]
+	public int Wait = 35;
+	public GameObject Text;
+
+	private int _count;
+
+	private void Update() {
+		var sum = _count / (double) Wait;
+		Text.GetComponent<TextMesh>().text = sum * 100 + "%";
+		Debug.Log(sum);
+	}
+
+	private void OnTriggerStay2D(Collider2D other) {
+		if (other.gameObject.CompareTag("Player")) {
+			_count += 1;
+		}
+		
+		if (other.gameObject.CompareTag("Player")) {
+			if (_count == Wait) {
+				SceneManager.LoadScene(Scene);
+			}
+		}
+	}
 
 	private void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.CompareTag("Player")) {
-			SceneManager.LoadScene(Scene);
+			_count = 0;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.CompareTag("Player")) {
+			_count = 0;
 		}
 	}
 }
