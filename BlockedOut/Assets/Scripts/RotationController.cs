9b63e49bed4a524a138da3f9119fa5c0e8aed3c6
Dibbo <DeflatedPickle@gmail.com>
2018-01-18 13:05:15 +0000

    Added smooth level rotation

--- BlockedOut/Assets/Scripts/RotationController.cs
+++ BlockedOut/Assets/Scripts/RotationController.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public class RotationController : MonoBehaviour {
 	private RotationManager _rotationManager;
 	private AudioSource _audioSource;
+	
+	private float _desiredRot;
+	[Range(1, 8)]
+	public float Damping = 3;
 
 	private void Awake() {
 		_rotationManager = GetComponent<RotationManager>();
 		_audioSource = GetComponent<AudioSource>();
 	}
-	
-	void Update () {
+ 
+	private void OnEnable() {
+		_desiredRot = transform.eulerAngles.z;
+	}
+
+	private void Update () {
 		if (Input.GetKeyDown(KeyCode.RightArrow)) {
-			_rotationManager.RotateRight();
+			// _rotationManager.RotateRight();
+			_desiredRot -= 90;
 			_audioSource.Play();
 		} else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-			_rotationManager.RotateLeft();
+			// _rotationManager.RotateLeft();
+			_desiredRot += 90;
 			_audioSource.Play();
 		}
+ 
+		var desiredRotQ = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, _desiredRot);
+		transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotQ, Time.deltaTime * Damping);
 	}
 }
